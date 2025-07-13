6d46c3d821a74f133b8298a19377d1b23d084637
Erdi-H <erdi@umich.edu>
2025-07-13 12:03:57 -0400

    feat: initialized components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { JobsList } from './components/jobs-list/jobs-list';
+import { Analytics } from './components/analytics/analytics';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: JobsList },
+    { path: 'analytics', component: Analytics }
+];
